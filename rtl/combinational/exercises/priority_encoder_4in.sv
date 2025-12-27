14b6852f805095f240f5cf6174249e1e140bac42
hitsan <15902694+hitsan@users.noreply.github.com>
2025-12-27 18:46:43 +0900

    fix valid

--- rtl/combinational/exercises/priority_encoder_4in.sv
+++ rtl/combinational/exercises/priority_encoder_4in.sv
@@ -6,6 +6,8 @@ module priority_encoder_4in #(
   output logic valid
 );
   always_comb begin
+    valid = 1'b1;  // デフォルトでvalid
+
     unique casez (data_in)
       4'b1???: data_out = 2'b11;
       4'b01??: data_out = 2'b10;
@@ -13,9 +15,10 @@ module priority_encoder_4in #(
       4'b0001: data_out = 2'b00;
       4'b0000: begin
         data_out = 2'b00;
-        valid = 0;
+        valid = 1'b0;
       end
     endcase
+  end
        
   // always_comb begin
   //   valid = 1'b1;
