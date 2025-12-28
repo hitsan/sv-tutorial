b30a802e6ad82086c276165b27ce757b10de478b
hitsan <15902694+hitsan@users.noreply.github.com>
2025-12-29 01:08:07 +0900

    impl multiplier

--- rtl/combinational/exercises/multiplier.sv
+++ rtl/combinational/exercises/multiplier.sv
@@ -3,6 +3,21 @@
 
 `timescale 1ns / 100ps
 
-module multiplier;
+module multiplier #(
+  parameter int INPUT_WIDTH = 8,
+  parameter bit IS_SIGNED = 1'b0,
+  localparam int OUTPUT_WIDTH = INPUT_WIDTH * 2
+) (
+  input logic [INPUT_WIDTH-1:0] in0,
+  input logic [INPUT_WIDTH-1:0] in1,
+  output logic [OUTPUT_WIDTH-1:0] product
+);
+
+  always_comb begin
+    case (IS_SIGNED)
+      1'b1: product = signed'(in0) * signed'(in1);
+      1'b0: product = in0 * in1;
+    endcase
+  end
 
 endmodule
