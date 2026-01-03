16ff17fe325c5c7931912dc83ad5ac07c16402ae
hitsan <15902694+hitsan@users.noreply.github.com>
2026-01-03 23:48:52 +0900

    rm files

--- 02_sequential/multiplier_pipelined/multiplier_pipelined_variants.sv
+++
@@ -1,6 +0,0 @@
-// パイプライン化された乗算器の各種実装
-// 2つの実装を別ファイルに分割しました。
-// - multiplier_pipelined_multistage.sv
-// - multiplier_pipelined_array.sv
-
-`timescale 1ns / 100ps
--- 02_sequential/multiplier_pipelined/multiplier_pipelined_variants_tb.sv
+++
@@ -1,6 +0,0 @@
-// パイプライン化された乗算器の各種実装 - テストベンチ
-// テストベンチは実装ごとに分割しました。
-// - multiplier_pipelined_multistage_tb.sv
-// - multiplier_pipelined_array_tb.sv
-
-`timescale 1ns / 100ps
