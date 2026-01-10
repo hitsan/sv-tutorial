82a29a60b9fdacf8cc6c784ea97f5ecb52cae9ab
hitsan <15902694+hitsan@users.noreply.github.com>
2026-01-11 02:46:08 +0900

    impl

--- 07_network/crc32/crc32.sv
+++ 07_network/crc32/crc32.sv
@@ -1,27 +1,55 @@
 // CRC32 計算モジュール
 // Ethernet CRC32 を LFSR で計算
+// ISO-HDLC (CRC-32) / IEEE 802.3 準拠
 
 `timescale 1ns / 1ps
 
 module crc32 #(
-    parameter logic [31:0] POLYNOMIAL = 32'h04C11DB7  // Ethernet CRC32多項式
+    parameter logic [31:0] POLYNOMIAL = 32'hEDB88320  // Ethernet CRC32多項式 (LSBファースト、ISO-HDLC)
 ) (
     input  logic        clk,
     input  logic        rst_n,
-    input  logic [7:0]  data_in,
+    input  logic [ 7:0] data_in,
     input  logic        valid_in,
-    input  logic        sof,         // Start of Frame
-    input  logic        eof,         // End of Frame
+    input  logic        sof,       // Start of Frame
+    input  logic        eof,       // End of Frame
     output logic [31:0] crc_out,
     output logic        crc_valid
 );
 
-    // TODO: CRCレジスタ
+  // TODO: CRCレジスタ
+  logic [31:0] crc_reg;
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) crc_reg <= 32'hFFFF_FFFF;
+    else if (valid_in & sof) begin
+      crc_reg <= calc_crc(32'hFFFF_FFFF, data_in);
+    end else if (sof) begin
+      crc_reg <= 32'hFFFF_FFFF;
+    end else if (valid_in) begin
+      crc_reg <= calc_crc(crc_reg, data_in);
+    end
+  end
+  assign crc_out = crc_reg ^ 32'hFFFF_FFFF;
+  // TODO: 8ビット入力に対するCRC計算
+  function automatic logic [31:0] calc_crc(logic [31:0] crc, logic [7:0] data);
+    logic [31:0] next_crc;
 
-    // TODO: 8ビット入力に対するCRC計算
+    next_crc = crc ^ {24'b0, data};
+    for (int i = 1; i <= 8; i++) begin
+      if (next_crc[0]) begin
+        next_crc = (next_crc >> 1) ^ POLYNOMIAL;
+      end else begin
+        next_crc = next_crc >> 1;
+      end
+    end
+    return next_crc;
+  endfunction
 
-    // TODO: フレーム境界でのレジスタ制御
-
-    // TODO: CRC出力
-
+  always_ff @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      crc_valid <= 0;
+    end else begin
+      crc_valid <= eof;
+    end
+  end
 endmodule
